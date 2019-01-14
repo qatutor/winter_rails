9949acc9c4db2d0abe094415793cd6f98e6dd099
Vadim Geroim <vadimgeroim.brooklyn@gmail.com>
2019-01-13 22:42:55 -0500

    update

---
+++ lesson31/31-2/customer_application.rb
@@ -0,0 +1,61 @@
+class CustomerApplication
+  def initialize(age, person, income, credit_history, credit)
+    @age = age
+    @person = person
+    @income = income
+    @credit_history = credit_history
+    @credit = credit
+  end
+
+  def calc_based_on_age
+    if @age >= 21 && @age <= 40
+      return @score += 10
+    end
+    if @age >40
+      return @score += 20
+    end
+  end
+
+  def calc_based_on_person
+    if @person == "женщина"
+      return @score += 10
+    end
+  end
+
+  def calc_based_on_income
+    if @income >= 20001 && @income <= 40000
+      return @score += 10
+    end
+    if @income > 40000
+      return @score += 20
+    end
+  end
+
+  def calc_based_on_credit_history
+    if @credit_history == "есть"
+      return @score += 20
+    end
+  end
+
+  def calc_based_on_credit
+    if @credit < 20000
+      return @score += 20
+    end
+    if @credit >= 20001  && @credit >= 40000
+      return @score += 10
+    end
+  end
+
+  def calculate_score
+    @score = 0
+    calc_based_on_age
+    calc_based_on_credit
+    calc_based_on_income
+    calc_based_on_person
+    calc_based_on_credit_history
+  end
+
+  def approved?
+    return @score >= 50
+  end
+end
---
+++ lesson31/31-2/main.rb
@@ -0,0 +1,25 @@
+require './customer_application.rb'
+
+puts "возраст: "
+age = gets.to_i
+
+puts "пол: "
+person = gets.chomp
+
+puts "доход: "
+income = gets.to_i
+
+puts "кредитная история (есть/нет)"
+credit_history = gets.chomp
+
+puts "запрашиваемая сумма: "
+credit = gets.to_i
+
+application = CustomerApplication.new(age, person, income, credit_history, credit)
+application.calculate_score
+
+if application.approved?
+  puts "Кредит подтвержден."
+else
+  puts "В кредите отказано."
+end
