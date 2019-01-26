6d544b15283ca54989b9daa94ed169b1c09b6d78
Vadim Geroim <vgeroim.qa@gmail.com>
2019-01-26 10:04:34 -0500

    update

--- lesson23/23_3/encrypt.rb
+++ lesson23/23_3/encrypt.rb
@@ -6,9 +6,19 @@ text = gets.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 
 encrypt_type = gets.to_i
 
-result = encrypt_type == 1 ? Digest::MD5.hexdigest(text) : Digest::SHA1.hexdigest(text)
+def encrypt(type, text)
+  case type
+  when 1
+    Digest::MD5.hexdigest(text)
+  when 2
+    Digest::SHA1.hexdigest(text)
+  when 3
+    Digest::SHA2.hexdigest(text)
+  end
+end
 
-puts "Результат: #{result}"
+puts "Результат: #{encrypt(encrypt_type, text)}"
