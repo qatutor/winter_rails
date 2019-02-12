4be8e345340616412756691480e91d22911598f8
Vadim Geroim <vgeroim.qa@gmail.com>
2019-02-12 00:55:21 -0500

    update

---
+++ lesson45/45-6/js-script/benefits.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('benefit-fresh-btn').click(alert("hello world"));
+
+
+    $('#benefit-fresh-btn').click(function () {
+        $(this).alert('You just clicked on me. Hey hey')
+    });
+
+});
